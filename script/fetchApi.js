f5a18c5d795be004af0b2ff3155e8aad86f3c726
Gabriel Alves <alvezinho.gabs@gmail.com>
2022-03-22 15:40:28 -0300

    conclusão de automação de funçoes

--- script/fetchApi.js
+++ script/fetchApi.js
@@ -1,17 +1,24 @@
+const acionarFuncao = () =>{
+    const getDiv = document.querySelectorAll('.caixa');
+    getDiv.forEach((elemento)=>{
+        const id = elemento.id;
+        const nome = elemento.firstElementChild.textContent;
+        const nomeConcat = elemento.firstElementChild.textContent.split('-').join('') // variável usada para acessar o objeto dentro do data
+        getValor(id, nomeConcat, nome)
+    })
+}
 
-const getValor = function(idDiv){
-    const getDiv = document.getElementById(idDiv);
-    const nomeMoeda = getDiv.firstElementChild.textContent.split('-').join(''); // variável usada para acessar o objeto dentro do data
+const getValor = function(idDiv, nomeConcat, nome){
     const options = {
         method: 'GET',
         mode: 'cors',
         cache: 'default'
     }
 
-    fetch(`https://economia.awesomeapi.com.br/last/${getDiv.firstElementChild.textContent}`, options)
+    fetch(`https://economia.awesomeapi.com.br/last/${nome}`, options)
         .then( response => { return response.json()
             .then( data => {
-                setValor(data[nomeMoeda], idDiv)
+                setValor(data[nomeConcat], idDiv)
             })
         })
         .catch(e => {window.alert('Não foi possível carregar os dados', e)})
@@ -22,15 +29,7 @@ const setValor = function(data,idDiv){
     for (elemento in data){
         if(document.querySelector(`#${idDiv} .${elemento}`)){
             document.querySelector(`#${idDiv} .${elemento}`).textContent = data[elemento]
-            console.log(document.querySelector(`#${idDiv} .${elemento}`))
         }
     }
 }
-
-
-getValor('div1')
-getValor('div2')
-getValor('div3')
-getValor('div4')
-getValor('div5')
-getValor('div6')
+setInterval(acionarFuncao, 1000)
