4d3fe556b44e222077a7e831427e14a76f96e8ed
Gabriel Alves <alvezinho.gabs@gmail.com>
2022-03-25 12:50:56 -0300

    novas páginas

--- script/convert.js
+++ script/convert.js
@@ -2,5 +2,5 @@ const mult = (idPar, idValue, idInput) =>{
     const valorPar = document.getElementById(idPar).value;
     const valorQuant = document.getElementById(idValue).value;
     const mult = Number(valorPar*valorQuant)
-    document.getElementById(idInput).textContent = mult.toFixed(2).replace('.',',')
+    document.getElementById(idInput).textContent = mult.toFixed(3).replace('.',',')
 }
