b92a6a18805ff49ef22f0df958e2685e21dea080
Gabriel Alves <99101659+Alvezin@users.noreply.github.com>
2022-07-14 17:36:21 -0300

    Inciando o processo de refatoração

--- script/convert.js
+++ script/convert.js
@@ -3,36 +3,28 @@ const mult = (idPar, idValue, idInput) =>{
     const nomeConcat = nomePar.split('-').join('')
     let a = nomePar.split('-')
     let quantiaFinal = a.splice(1, 1)
-    if (idPar === 'inputParCompra'){
-        fetchCompra(nomePar, nomeConcat, idValue, idInput, quantiaFinal)
-    } else if (idPar === 'inputParVenda'){
-        fetchCompra(nomePar, nomeConcat, idValue, idInput, quantiaFinal)
-    }
-    
+    fetchCompra(nomePar, nomeConcat, idValue, idInput, quantiaFinal)
 }
 
-const fetchCompra = (nomePar, nomeConcat, idValue, idInput, quantiaFinal) =>{
-    axios.get(`https://economia.awesomeapi.com.br/last/${nomePar}`)
-    .then( response => {
-            if (response.data[nomeConcat] !== undefined){
-                if (idValue == 'inputValueCompra'){
-                    
-                    const valorUnitCompra = document.getElementById('parCompra')
-                    valorUnitCompra.textContent =  response.data[nomeConcat].bid;
+const fetchCompra = async (nomePar, nomeConcat, idValue, idInput, quantiaFinal) =>{
+    const req = await axios.get(`https://economia.awesomeapi.com.br/last/${nomePar}`)
+    if (req.data[nomeConcat] !== undefined){
+        if (idValue == 'inputValueCompra'){
+            
+            const valorUnitCompra = document.getElementById('parCompra')
+            valorUnitCompra.textContent =  req.data[nomeConcat].bid;
 
-                    const valorQuant = document.getElementById(idValue).value;
-                    const mult = Number(valorUnitCompra.textContent * valorQuant)
-                    document.getElementById(idInput).textContent = `${mult.toFixed(5)} ${quantiaFinal}`
-                }else{
+            const valorQuant = document.getElementById(idValue).value;
+            const mult = Number(valorUnitCompra.textContent * valorQuant)
+            document.getElementById(idInput).textContent = `${mult.toFixed(5)} ${quantiaFinal}`
+        }else{
 
-                    const valorUnitVenda = document.getElementById('parVenda')
-                    valorUnitVenda.textContent = response.data[nomeConcat].ask;
+            const valorUnitVenda = document.getElementById('parVenda')
+            valorUnitVenda.textContent = req.data[nomeConcat].ask;
 
-                    const valorQuant = document.getElementById(idValue).value;
-                    const mult = Number(valorUnitVenda.textContent * valorQuant)
-                    document.getElementById(idInput).textContent = `${mult.toFixed(5)} ${quantiaFinal}`
-                }
-            }
-    })
-    .catch(e => {window.alert('Alguns dados não foram totalmente carregados', e)})
+            const valorQuant = document.getElementById(idValue).value;
+            const mult = Number(valorUnitVenda.textContent * valorQuant)
+            document.getElementById(idInput).textContent = `${mult.toFixed(5)} ${quantiaFinal}`
+        }
+    }
 }
